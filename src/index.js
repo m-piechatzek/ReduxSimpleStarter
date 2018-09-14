493bef547496ae88f60678ee65584ccf4f90b594
MonikaP <piechatzek@hotmail.com>
2018-09-14 17:47:07 -0600

    added list of vids

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import YTSearch from 'youtube-api-search'
 
 import SearchBar from './components/search_bar'
+import VideoList from './components/video_list'
 
 const API_KEY = 'AIzaSyDJowKxUE5Ooe6U36I0MHvXBgVHevncMV4'
 
@@ -28,6 +29,7 @@ class App extends React.Component {
 			<div>
 				hiiii
 				<SearchBar />
+				<VideoList videos={this.state.videos} />
 			</div>
 		)
 	}
